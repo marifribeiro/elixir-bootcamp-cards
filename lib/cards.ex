e82c3a463bba95be3eb751d6dacc2e27356ee518
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-09-23 17:45:26 -0300

    Tests and documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creatind and handling a deck of cards.
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of cards.
+  """
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits, value <- values do
@@ -12,25 +19,60 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks if a deck contains a provided card (i.e. "Ace of Spades").
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves a deck into a file.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads a file that contains a deck (saved with the `save` function).
+  """
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "File not found"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "File not found"
     end
+  end
+
+  @doc """
+    Creates a deck, shuffles it and deals it using the provided `hand_size`
+  """
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,29 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 52 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "deal keeps the right number of remainder cards" do
+    deck = Cards.create_deck
+    hand_size = 5
+    remainder = length(deck) - hand_size
+
+    {_hand, rest_of_deck} = Cards.deal(deck, hand_size)
+    assert length(rest_of_deck) == remainder
+  end
+
+  test "deal gives the right amount of cards" do
+    deck = Cards.create_deck
+    hand_size = 9
+    {hand, _rest_of_deck} = Cards.deal(deck, hand_size)
+    assert length(hand) == hand_size
   end
 end
